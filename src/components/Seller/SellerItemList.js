9e6a0f9556f9cbd2eb49f16f7499e9c5324dee17
Jonathan Mendoza <53712048+Jnmendza@users.noreply.github.com>
2020-05-22 03:47:57 -0700

    Update SellerItemList.js

--- src/components/Seller/SellerItemList.js
+++ src/components/Seller/SellerItemList.js
@@ -57,11 +57,16 @@ class SellerItemList extends React.Component {
     }
 }
 
+//2.a tells connect which pieces of state to pass to the component
+//2.b takes state (whatever the reducer is returning)
 const mapStateToProps = (state) => ({
+    //2.c the properties (left hand-side:) returned are what gets passed as props
+    //2.d the values (:right hand-side)returned are properties from the state object(reducer)
     items: state.items
 })
 
-export default withRouter(connect(
-    mapStateToProps,
-    { getItemsUserId }
-)(SellerItemList));
+//1.c Instead of exporting component, export connect func
+//1.d Inovke connect twice
+//  - First call, pass in a mapStateToProps function and object(action)
+//  - Second call, pass in component being connected
+export default withRouter(connect(mapStateToProps,{getItemsUserId})(SellerItemList));
